fcb2b40a3c1438d132f5748be1bb3dbdeaf5dc28
rimjhimsudo <rimjhimvaish@gmail.com>
2020-05-11 18:11:31 +0530

    dalog box

--- app/src/main/java/com/example/todoapp/MyNotesActivity.java
+++ app/src/main/java/com/example/todoapp/MyNotesActivity.java
@@ -1,10 +1,14 @@
 package com.example.todoapp;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -24,9 +28,19 @@ public class MyNotesActivity extends AppCompatActivity {
         btn_float.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                setupdialogbox();
             }
         });
+
+    }
+
+    private void setupdialogbox(){
+        View view= LayoutInflater.from(MyNotesActivity.this).inflate(R.layout.addnoteslayout,null);
+        EditText editTextTtle=view.findViewById(R.id.et_title);
+        EditText editTextdesc=view.findViewById(R.id.et_desc);
+        Button btn_submt=view.findViewById(R.id.btn_submtnote);
+        AlertDialog dialog=new AlertDialog.Builder(this).setView(view).setCancelable(false).create();
+        dialog.show();
 
     }
 }
