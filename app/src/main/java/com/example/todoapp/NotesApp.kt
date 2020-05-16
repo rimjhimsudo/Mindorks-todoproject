c013c5f0ba82e8984eb200ba1947724343b67ced
rimjhimsudo <rimjhimvaish@gmail.com>
2020-05-16 16:41:34 +0530

    database implemented

--- app/src/main/java/com/example/todoapp/NotesApp.kt
+++ app/src/main/java/com/example/todoapp/NotesApp.kt
@@ -1,9 +1,14 @@
 package com.example.todoapp
 
 import android.app.Application
+import com.example.todoapp.db.NotesDatabase
 
 class  NotesApp : Application(){
     override fun onCreate() {
         super.onCreate()
+    }
+
+    fun getNotesDb():NotesDatabase{
+        return NotesDatabase.getInstance(this)
     }
 }
--- app/src/main/java/com/example/todoapp/db/NotesDao.kt
+++ app/src/main/java/com/example/todoapp/db/NotesDao.kt
@@ -1,2 +1,18 @@
 package com.example.todoapp.db
 
+import androidx.room.*
+import androidx.room.OnConflictStrategy.REPLACE
+
+//data access objects - dao
+
+@Dao
+interface NotesDao{
+    @Query("SELECT * from notesData")
+    fun getAll(): List<Notes>
+    @Insert(onConflict = REPLACE)
+    fun insert(notes:Notes)
+    @Update
+    fun updateNotes(notes: Notes)
+    @Delete
+    fun delete(notes: Notes)
+}
